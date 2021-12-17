6a82d34327b9c019261e57929ba334f308e6f552
JohnGupta-She <jgupta-she30@stuy.edu>
2021-12-17 14:03:07 -0500

    class update

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,3 +1,9 @@
 public class SearchDriver {
+  ArrayList <Integers> test = new ArrayList <Integers>();
+  test.add(0);
+  for(int i = 1; i < 350000000000; i++){
+    test.add( test.get(i-1) + (int)(10 * Math.random));
+  }
+
   
 }
