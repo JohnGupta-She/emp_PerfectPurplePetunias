4e7e8c50913eb5e417b9caf758a27c0fa024e681
eortiz-30 <eortiz30@stuy.edu>
2021-12-20 14:03:37 -0500

    2021-12-20 in class

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,6 +1,5 @@
 public class SearchDriver {
 
-
   private static String toString(int[] input){
     String output = "";
     for (int x: input){
@@ -9,24 +8,35 @@ public class SearchDriver {
     return output;
   }
 
-  public static void main(String[] args){
-    int[] test;
-    test = new int[200000000];
-    for(int i = 0; i < 200000000; i++){
+  private static void fillTest(int x, int[] test){
+    test = new int[x];
+    for(int i = 0; i < x; i++){
       test[i] = i;
-      //System.out.println(test[i]);
     }
+  }
 
-    int[] target = new int[10];
-    for(int x = 0; x < 10; x++){
-      target[x] = (int)(Math.random() * 200000000);
-    }
+  public static void main(String[] args){
+    int[] test = {};
+    /*
+    10,000,000
+    20,000,000
+    100,000,000
+    200,000,000
+    1,000,000,000
+    2,000,000,000
+    */
+    int[] arraylengths = {10000000, 20000000, 100000000, 200000000};
+
 
     System.out.println("LinSearch Test");
-    for(int trialCtr = 0; trialCtr < 10; trialCtr ++){
-      System.out.println("Trial " + trialCtr);
-      System.out.println(target[trialCtr]);
-      System.out.println(LinSearch.linSearch(test, target[trialCtr]));
+    for (int x = 0; x < 100; x++){
+      for(int trialCtr = 0; trialCtr < 4; trialCtr ++){
+        fillTest(arraylengths[trialCtr], test);
+        System.out.println("Array Length " + test[trialCtr]);
+        for (int x = 0; x < 100; x++){
+          LinSearch.linSearch(test, test.length-1);
+        }
+      }
     }
     /*
     System.out.println("BinSearch Test");
